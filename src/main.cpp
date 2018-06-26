c3cb336509243e31fc82765b22c2f093fae79cc2
leesj9476 <leesj9476@naver.com>
2018-06-27 01:31:34 +0900

    180627 ver 1.2.1

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <unistd.h>
 #include <csignal>
+#include <thread>
+#include <chrono>
 
 #include "ssd1306_i2c.h"
 
@@ -15,8 +17,6 @@ using namespace cv;
 
 double *lens_mat[5][5];
 
-void int_handler(int);
-
 int main () {
 	ifstream dup_lock("./lock/dup_lock");
 	if (dup_lock.is_open())
@@ -25,27 +25,27 @@ int main () {
 		system("touch ./lock/dup_lock");
 
 	// options setting information
-	Options options = parseSettingFile();
+	Options opt = parseSettingFile();
 	makeLensMatrix();
 
-	// Initiate i2c led
-	ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
-	ssd1306_setTextSize(1);
-	ssd1306_clearDisplay();
-	ssd1306_display();
-
-	// Ctrl-c interrupt(kill -9 <pid>)
-	signal(SIGINT, int_handler);
-
 	// image mode
-	if (options.option[INPUT_IMAGE_FILE]) {
-		Image image(options);
+	if (opt.option[INPUT_IMAGE_FILE]) {
+		Image image(opt);
 		image.init();
 		cout << image.findAllPoints() << endl;
 	}
 	// cam mode
 	else {
-		Capture cam(options);
+		Capture cam(opt);
+
+		// Initiate i2c led
+		ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
+		ssd1306_setTextSize(1);
+		ssd1306_display();
+		this_thread::sleep_for(chrono::milliseconds(1000));
+
+		// Ctrl-c interrupt(kill -9 <pid>)
+		signal(SIGINT, int_handler);
 
 		if (!cam.isValid()) {
 			cerr << "<error> camera open failed" << endl;
